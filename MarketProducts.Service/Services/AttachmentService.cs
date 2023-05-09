5ec154ce894fef0a983cf346f2105d290403e6f4
= <olimxonnutfullaevich@gmail.com>
2023-05-09 18:57:50 +0500

    SERVICE: Attachment service for Update

--- MarketProducts.Service/Services/AttachmentService.cs
+++ MarketProducts.Service/Services/AttachmentService.cs
@@ -1,8 +1,11 @@
-﻿using MarketProducts.Domain.Entities.Attachments;
+﻿using MarketProducts.Data.Repositories;
+using MarketProducts.Domain.Entities.Attachments;
 using MarketProducts.Service.DTOs;
+using MarketProducts.Service.Exceptions;
 using MarketProducts.Service.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,14 +14,39 @@ namespace MarketProducts.Service.Services
 {
     public class AttachmentService : IAttachmentService
     {
-        public Task<bool> DeleteAsync(int id)
+        private readonly AttachmentRepository attachmentRepository;
+
+        public AttachmentService(AttachmentRepository attachmentRepository)
         {
+            this.attachmentRepository = attachmentRepository;
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            //var photo = await _attachmentService.FindAsync(id);
             throw new NotImplementedException();
         }
 
-        public Task<Attachment> UpdateAsync(AttachmentForCreationDTO dto)
+        public async Task<Attachment> UpdateAsync(AttachmentForCreationDTO dto)
         {
-            throw new NotImplementedException();
+            var existAttachment = await attachmentRepository.GetAsync();
+
+            if (existAttachment != null)
+                throw new MarketException(404, "Attachment not found");
+
+            string fileName = existAttachment.Path;
+            string filePath = Path.Combine(dto.Path, fileName);
+
+            // copy image to the destination as stream
+            FileStream fileStream = File.OpenWrite(filePath);
+            //await dto.CopyToAsync(fileStream);
+
+            await fileStream.FlushAsync();
+            fileStream.Close();
+
+            await attachmentRepository.SaveChangesAsync();
+
+            return existAttachment;
         }
 
         public Task<Attachment> UploadAsync(AttachmentForCreationDTO dto)
